9e2d198f6bd1ba8972d6f409ef063a048734088e
Prof. J. L. Gregório <jlgregorio81@hotmail.com>
2025-09-19 09:02:40 -0300

    Documentation of CategoryController - Create and Find

--- src/main/java/br/com/jlgregorio/Products/controller/CategoryController.java
+++ src/main/java/br/com/jlgregorio/Products/controller/CategoryController.java
@@ -1,10 +1,16 @@
 package br.com.jlgregorio.Products.controller;
 
 import br.com.jlgregorio.Products.dto.CategoryDto;
+import br.com.jlgregorio.Products.exceptions.ResourceNotFoundException;
 import br.com.jlgregorio.Products.model.CategoryModel;
 import br.com.jlgregorio.Products.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,36 +24,56 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDto>> findAll(){
+    public ResponseEntity<List<CategoryDto>> findAll() {
         List<CategoryDto> categories = categoryService.findAll();
         return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
-    @PostMapping
-    public ResponseEntity<CategoryDto> create(@RequestBody CategoryDto categoryDto){
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(description = "Cria uma categoria", responses = {
+            @ApiResponse(description = "A nova categoria cadastrada", responseCode = "201",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = CategoryDto.class))
+            )
+    })
+    public ResponseEntity<CategoryDto> create(@RequestBody CategoryDto categoryDto) {
         CategoryDto created = categoryService.create(categoryDto);
         return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<CategoryDto> findById(@PathVariable("id") long id){
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(description = "Faz uma pesquisa por uma instancia de Category mediante o ID informado",
+            responses = {
+                    @ApiResponse(description = "Retorna um objeto Category", responseCode = "200",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = CategoryDto.class))
+                    ),
+                    @ApiResponse(description = "Retornar uma exceção caso não encontre",
+                            responseCode = "404", content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ResourceNotFoundException.class))
+                    )
+
+            }
+    )
+    public ResponseEntity<CategoryDto> findById(@PathVariable("id") long id) {
         CategoryDto found = categoryService.findById(id);
         return new ResponseEntity<>(found, HttpStatus.OK);
     }
 
     @PutMapping
-    public ResponseEntity<CategoryDto> update(@RequestBody CategoryDto categoryDto){
+    public ResponseEntity<CategoryDto> update(@RequestBody CategoryDto categoryDto) {
         var updated = categoryService.update(categoryDto);
         return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable long id){
+    public ResponseEntity<?> delete(@PathVariable long id) {
         categoryService.delete(id);
         return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
     }
-
-
 
 
 }
