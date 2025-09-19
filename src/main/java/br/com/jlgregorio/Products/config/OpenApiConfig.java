9525e6df308343fde186136bc4de116b114b9491
Prof. J. L. Gregório <jlgregorio81@hotmail.com>
2025-09-19 08:33:37 -0300

    Basic of Documentation

---
+++ src/main/java/br/com/jlgregorio/Products/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package br.com.jlgregorio.Products.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My Store Documentation")
+                        .version("1.0")
+                        .description("Documentação da API MyStore")
+                        .termsOfService("www.jlgregorio.com.br")
+                        .license(
+                                new License()
+                                        .name("Apache License 2.0")
+                                        .url("https://www.apache.org/licenses/LICENSE-2.0")
+                        )
+                );
+    }
+
+}
